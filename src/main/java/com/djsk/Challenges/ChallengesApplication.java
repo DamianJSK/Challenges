059beed5cd60cc3689bbe449ec587f9dacb6b172
Damian <dmn.jsk@gmail.com>
2018-01-22 18:24:02 +0100

    init

---
+++ src/main/java/com/djsk/Challenges/ChallengesApplication.java
@@ -0,0 +1,12 @@
+package com.djsk.Challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengesApplication.class, args);
+	}
+}
