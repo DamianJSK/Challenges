afb7dc40404d0a94e793524b935cf12ec42c1f42
Damian <dmn.jsk@gmail.com>
2018-01-25 18:16:29 +0100

    basic config: postgre, tomcat

--- src/main/java/com/djsk/Challenges/ChallengesApplication.java
+++ src/main/java/com/djsk/Challenges/ChallengesApplication.java
@@ -3,6 +3,8 @@ package com.djsk.Challenges;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableJpaRepositories("com.djsk.Challenges.persistence.dao")
+//@EntityScan("com.djsk.Challenges.persistence.entity")
 @SpringBootApplication
 public class ChallengesApplication {
 
