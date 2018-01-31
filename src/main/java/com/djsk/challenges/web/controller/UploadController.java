0e0a8bb83c47fe4655c86c1215e0ade5726a71cb
Damian <dmn.jsk@gmail.com>
2018-01-31 20:27:04 +0100

    Logger logging example

--- src/main/java/com/djsk/challenges/web/controller/UploadController.java
+++ src/main/java/com/djsk/challenges/web/controller/UploadController.java
@@ -3,6 +3,8 @@ package com.djsk.challenges.web.controller;
 import com.djsk.challenges.business.service.IDifferentOperationService;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,8 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
     @Autowired
     IDifferentOperationService differentOperationService;
@@ -25,5 +29,14 @@ public class UploadController {
             throw new FileUploadException("Invalid file format - must be jpg");
         }
         differentOperationService.uploadFile(file);
+    }
+
+    @GetMapping("/logger_test")
+    public void getLog(){
+        logger.warn("This is warning from: " + this.getClass());
+        logger.info("You can log something");
+        logger.warn("You can log something");
+        logger.debug("You can log something");
+        logger.error("You can log something");
     }
 }
